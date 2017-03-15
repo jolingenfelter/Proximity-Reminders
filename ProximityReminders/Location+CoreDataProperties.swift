6a2fb125563fb9a831b84b03a4586b499782cc13
Joanna Lingenfelter <lingenfelter.joanna@gmail.com>
2017-03-15 17:41:36 -0400

    Methods for ManagedObject subclasses

--- ProximityReminders/Location+CoreDataProperties.swift
+++ ProximityReminders/Location+CoreDataProperties.swift
@@ -9,13 +9,31 @@
 import Foundation
 import CoreData
 
-
 extension Location {
-
+    
+    static let entityName = "\(Location.self)"
+    
     @nonobjc public class func fetchRequest() -> NSFetchRequest<Location> {
         return NSFetchRequest<Location>(entityName: "Location");
     }
+    
+    class func location(withLatitude latitude: Double, longitude: Double) -> Location {
+        
+        let location = NSEntityDescription.insertNewObject(forEntityName: Location.entityName, into: CoreDataStack.sharedInstance.managedObjectContext) as! Location
+        
+        location.latitude = latitude
+        location.longitude = longitude
+        
+        return location
+        
+    }
 
+    
+}
+
+
+extension Location {
+    
     @NSManaged public var latitude: Double
     @NSManaged public var longitude: Double
     @NSManaged public var reminder: Reminder?
--- ProximityReminders/Reminder+CoreDataProperties.swift
+++ ProximityReminders/Reminder+CoreDataProperties.swift
@@ -8,18 +8,57 @@
 
 import Foundation
 import CoreData
+import CoreLocation
+
+extension Reminder {
+    
+    static let entityName = "\(Reminder.self)"
+    
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Reminder> {
+        let request = NSFetchRequest<Reminder>(entityName: Reminder.entityName)
+        request.sortDescriptors = [NSSortDescriptor(key: "date", ascending: true)]
+        return request;
+    }
+    
+    class func reminder(withText text: String) -> Reminder {
+        
+        let reminder = NSEntityDescription.insertNewObject(forEntityName: Reminder.entityName, into: CoreDataStack.sharedInstance.managedObjectContext) as! Reminder
+        
+        reminder.date = Date()
+        reminder.text = text
+        
+        return reminder
+    }
+    
+    class func reminder(withText text: String, andLocation location: CLLocation) -> Reminder {
+        
+        let reminder = Reminder.reminder(withText: text)
+        reminder.addLocation(location: location)
+        
+        return reminder
+        
+    }
+    
+    func addLocation(location: CLLocation?) {
+        
+        if let location = location {
+            
+            let reminderLocation = Location.location(withLatitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+            
+            self.location = reminderLocation
+        }
+        
+    }
+    
+}
 
 
 extension Reminder {
 
-    @nonobjc public class func fetchRequest() -> NSFetchRequest<Reminder> {
-        return NSFetchRequest<Reminder>(entityName: "Reminder");
-    }
-
     @NSManaged public var text: String?
     @NSManaged public var completed: Bool
     @NSManaged public var id: String?
-    @NSManaged public var date: NSDate?
+    @NSManaged public var date: Date?
     @NSManaged public var location: Location?
 
 }
