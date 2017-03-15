91c5e5740bf2c5495ab819addb6a836937b26335
Joanna Lingenfelter <lingenfelter.joanna@gmail.com>
2017-03-15 17:50:51 -0400

    RemindersFetchedResultsController

---
+++ ProximityReminders/ProximityReminders/RemindersFetchedResultsController.swift
@@ -0,0 +1,47 @@
+//
+//  RemindersFetchedResultsController.swift
+//  ProximityReminders
+//
+//  Created by Joanna Lingenfelter on 3/15/17.
+//  Copyright © 2017 JoLingenfelter. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class RemindersFetchedResultsController: NSFetchedResultsController<NSFetchRequestResult>, NSFetchedResultsControllerDelegate {
+    
+    private let tableView: UITableView
+    
+    init(fetchRequest: NSFetchRequest<NSFetchRequestResult>, managedObjectContext: NSManagedObjectContext, tableView: UITableView) {
+        
+        self.tableView = tableView
+        
+        super.init(fetchRequest: fetchRequest, managedObjectContext: CoreDataStack.sharedInstance.managedObjectContext, sectionNameKeyPath: nil, cacheName: nil)
+        
+        self.delegate = self
+        
+    }
+    
+    func executeFetch() {
+        
+        do {
+            
+            try performFetch()
+            
+        } catch let error as NSError {
+            
+            print("Unresolved error: \(error.localizedDescription)")
+            
+        }
+        
+    }
+    
+    
+    // MARK: - NSFetchedResultsController Delegate
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.reloadData()
+    }
+
+}
